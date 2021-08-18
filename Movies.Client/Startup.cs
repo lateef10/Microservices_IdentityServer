875c22456caa58fee89556eaad97787b97e89efc
Default Name <lateefrasheed10@gmail.com>
2021-08-17 22:13:37 -0400

    identity server

--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,8 +22,51 @@ namespace Movies.Client
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
             services.AddScoped<IMovieApiService, MovieApiService>();
+
+
+            services.AddHttpClient();
+            services.AddHttpContextAccessor();
+
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = "https://localhost:5005";
+
+                    options.ClientId = "movies_mvc_client";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code id_token";
+
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    /*options.Scope.Add("address");
+                    options.Scope.Add("email");
+                    options.Scope.Add("roles");*/
+
+                    /*options.ClaimActions.DeleteClaim("sid");
+                    options.ClaimActions.DeleteClaim("idp");
+                    options.ClaimActions.DeleteClaim("s_hash");
+                    options.ClaimActions.DeleteClaim("auth_time");
+                    options.ClaimActions.MapUniqueJsonKey("role", "role");
+
+                    options.Scope.Add("movieAPI");*/
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    /*options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = JwtClaimTypes.GivenName,
+                        RoleClaimType = JwtClaimTypes.Role
+                    };*/
+                });
 
         }
 
@@ -43,6 +88,7 @@ namespace Movies.Client
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
